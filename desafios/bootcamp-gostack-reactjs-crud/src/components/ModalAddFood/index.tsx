7ffa643edb584d7ad38c90a69628fddcd6843944
Daniel Felizardo <daniel.david772@gmail.com>
2020-08-02 14:01:33 -0300

    :pizza: Finished Challenge - Gostack - Reactjs - Crud

---
+++ desafios/bootcamp-gostack-reactjs-crud/src/components/ModalAddFood/index.tsx
@@ -0,0 +1,85 @@
+import React, { useRef, useCallback } from 'react';
+
+import { FiCheckSquare } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+interface IFoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+interface ICreateFoodData {
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+}
+
+interface IModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food: Omit<IFoodPlate, 'id' | 'available'>) => void;
+}
+
+const ModalAddFood: React.FC<IModalProps> = ({
+  isOpen,
+  setIsOpen,
+  handleAddFood,
+}) => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async (data: ICreateFoodData) => {
+      try {
+        const schema = Yup.object().shape({
+          image: Yup.string().required(),
+          name: Yup.string().required(),
+          price: Yup.number().required(),
+          description: Yup.string().min(
+            3,
+            'A descrição precisa ter no mínimo 3 caracteres.',
+          ),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        handleAddFood(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsOpen();
+      }
+    },
+    [handleAddFood, setIsOpen],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalAddFood;
